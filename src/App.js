374ebafab4de662d5f56afc501ad7d3eabc01298
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-08 13:02:46 +0300

    Adds src/App.js src/Article.js src/Comment.js src/Heading1.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Heading1 from "./Heading1";
 
 function App() {
   return (
     <div>
+      <Heading1 />
       <Article />
       <Comment />
     </div>
---
+++ src/Heading1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HeadingOne () {
+    return (
+    <h1>Facts!</h1>
+    );
+}
+
+export default HeadingOne;
